8352341b93b9b20b05736f55e5d4de983df264ae
Kate Dinh <kqtednh@gmail.com>
2024-04-07 13:36:21 -0700

    Initial Commit

--- log.php
+++ log.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'login.php';
-$connection = new mysqli($hn, $un, $pw, $db);
-if ($connection->connect_error) die ( $connection->connect_error);
+    require_once 'login.php';
+    $connection = new mysqli($hn, $un, $pw, $db);
+    if ($connection->connect_error) die ( $connection->connect_error);
     return $connection;
 ?>
